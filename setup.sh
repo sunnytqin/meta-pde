ecb48b6dca5377e958a8def1588e283461f93640
Alex Upjohn Beatson <abeatson@wash.cs.princeton.edu>
2021-05-14 06:08:11 -0400

    change setup.sh to fix bug on cluster with mismatch tf,protobuf,hdfs versions

--- setup.sh
+++ setup.sh
@@ -6,12 +6,11 @@ conda create -n meta-pde python==3.8
 conda activate meta-pde
 # conda install -c conda-forge dolfin-adjoint==2019.1.0
 conda install -c conda-forge mshr=2019.1.0=*_4
-conda install -c conda-forge hdf5==1.10.4
 conda install -c conda-forge numpy scipy matplotlib==3.0.3 jupyter
+conda install -c conda-forge tensorflow
 conda install cudatoolkit==11.1
 conda install cudnn==8
 pip install --upgrade pip
-pip install --upgrade tensorflow
 pip install --upgrade jax jaxlib==0.1.65+cuda112 -f https://storage.googleapis.com/jax-releases/jax_releases.html
 pip install flax
 pip install git+https://github.com/nestordemeure/flaxOptimizers.git
