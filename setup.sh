45da2e44434ad5270174072218d376b0213e6bb8
Sunny Qin <sunny.tqin@gmail.com>
2023-12-25 23:51:13 -0500

    update setup script

--- setup.sh
+++ setup.sh
@@ -4,17 +4,19 @@ conda deactivate
 conda remove --name meta-pde --all
 conda create -n meta-pde python==3.9
 conda activate meta-pde
-# conda install -c conda-forge dolfin-adjoint==2019.1.0
-conda install -c conda-forge mshr=2019.1.0=*_4
-conda install -c conda-forge scipy
+conda install -c conda-forge mshr=2019.1.0=py39h9e2e2ce_9
+conda install -c conda-forge scipy=1.11.4
 conda install -c conda-forge matplotlib
-#conda install -c conda-forge jupyter
-conda install -c conda-forge tensorflow
-conda install cudatoolkit==11.1
+conda install -c conda-forge tensorflow==2.15.0  
+conda install cudatoolkit==12
 conda install cudnn==8
 pip install --upgrade pip
 pip install --upgrade jax==0.2.20
-pip install --upgrade jaxlib==0.1.69+cuda111 -f https://storage.googleapis.com/jax-releases/jax_releases.html
+# pip install --upgrade jaxlib==0.1.69+cuda111 -f https://storage.googleapis.com/jax-releases/jax_releases.html
+pip install --upgrade jaxlib==0.1.75 -f https://storage.googleapis.com/jax-releases/jax_releases.htmlpip 
 pip install flax==0.3.3
-pip install git+https://github.com/nestordemeure/flaxOptimizers.git
 
+## Trouble shoot
+# if you encounter error: ImportError: /usr/lib/aarch64-linux-gnu/libstdc++.so.6: version `GLIBCXX_3.4.30' not found
+# do the following in command line
+# export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:<your path to conda environment>/envs/meta-pde/lib
