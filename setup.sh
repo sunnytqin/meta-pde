85a39498288e602184837b21352369c3ccc2fc79
Sunny Qin <120140471+sunnytqin@users.noreply.github.com>
2024-01-05 17:51:49 -0500

    Update setup.sh
    small clarifications

--- setup.sh
+++ setup.sh
@@ -4,16 +4,16 @@ conda deactivate
 conda remove --name meta-pde --all
 conda create -n meta-pde python==3.9
 conda activate meta-pde
-conda install -c conda-forge mshr=2019.1.0=py39h9e2e2ce_9
+conda install -c conda-forge mshr=2019.1.0=*_9 # a build that worked for me on linux py39h9e2e2ce_9 but may depend on your system 
 conda install -c conda-forge scipy=1.11.4
 conda install -c conda-forge matplotlib
-conda install -c conda-forge tensorflow==2.15.0  
-conda install cudatoolkit==12
-conda install cudnn==8
+# conda install cudatoolkit==12 should be automatically installed when you install jax/tensorlow. check your CUDA version
+# conda install cudnn==8
 pip install --upgrade pip
+python3 -m pip install tensorflow[and-cuda] # version 2.15.0  
 pip install --upgrade jax==0.2.20
 # pip install --upgrade jaxlib==0.1.69+cuda111 -f https://storage.googleapis.com/jax-releases/jax_releases.html
-pip install --upgrade jaxlib==0.1.75 -f https://storage.googleapis.com/jax-releases/jax_releases.htmlpip 
+pip install --upgrade jaxlib==0.1.75 -f https://storage.googleapis.com/jax-releases/jax_releases.html  
 pip install flax==0.3.3
 
 ## Trouble shoot
